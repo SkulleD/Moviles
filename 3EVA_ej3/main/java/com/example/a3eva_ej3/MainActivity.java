0c04680eb612baa2f3dc559247b4f43f6fcabec7
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-04-23 13:59:31 +0200

    Add files via upload

--- 3EVA_ej3/main/java/com/example/a3eva_ej3/MainActivity.java
+++ 3EVA_ej3/main/java/com/example/a3eva_ej3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a3eva_ej3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -20,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
     CheckBox checkBox;
     RadioGroup radioGroup;
     RadioButton radioButton;
+    RadioButton radioButton2;
+    RadioButton radioButton3;
     TextView txtCheck;
+    DatePicker datePicker;
+    EditText txtCalendar;
     int valor;
 
     @Override
@@ -36,19 +42,21 @@ public class MainActivity extends AppCompatActivity {
         txtCheck = findViewById(R.id.textView2);
         radioGroup = findViewById(R.id.radioGroup);
         radioButton = findViewById(R.id.radioButton);
-        radioButton = findViewById(R.id.radioButton2);
-        radioButton = findViewById(R.id.radioButton3);
-        //DatePicker = findViewById(R.id.editTextDate);
+        radioButton2 = findViewById(R.id.radioButton2);
+        radioButton3 = findViewById(R.id.radioButton3);
+        datePicker = findViewById(R.id.datePicker);
+        txtCalendar = findViewById(R.id.editTextDate);
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!checkBox.isChecked()) {
-                    txtBoton.setText((valor++) + "");
+                    valor++;
+                    txtBoton.setText((valor) + "");
                 } else {
-                    txtBoton.setText((valor--) + "");
+                    valor--;
+                    txtBoton.setText((valor) + "");
                 }
-
             }
         });
 
@@ -58,10 +66,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (flag) {
-                    botonggle.setBackgroundColor(Color.WHITE);
+                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
                 } else {
-                    botonggle.setBackgroundColor(Color.GRAY);
+                    getWindow().getDecorView().setBackgroundColor(Color.GRAY);
                 }
+                flag = !flag;
             }
         });
 
@@ -70,9 +79,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (checkBox.isChecked()) {
                     boton.setText("RESTAR");
+                    checkBox.setText("Cambiar a Suma");
                 } else {
                     boton.setText("SUMAR");
+                    checkBox.setText("Cambiar a Resta");
                 }
+            }
+        });
+
+        radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCheck.setText("1º seleccionado");
+            }
+        });
+
+        radioButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCheck.setText("2º seleccionado");
+            }
+        });
+
+        radioButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCheck.setText("3º seleccionado");
+            }
+        });
+
+        datePicker.init(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(), new DatePicker.OnDateChangedListener(){
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                txtCalendar.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
             }
         });
     }
