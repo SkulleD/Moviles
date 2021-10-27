419ec024e6fd3530ebd238b0f4462f663edb4972
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-10-27 18:22:55 +0200

    Add files via upload

---
+++ main/java/com/alvarovila/componentes/Terciaria.java
@@ -0,0 +1,17 @@
+package com.alvarovila.componentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Terciaria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terciaria);
+
+        Intent intent = getIntent();
+    }
+}
